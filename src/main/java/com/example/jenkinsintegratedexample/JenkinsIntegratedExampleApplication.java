88a2635b0c7f9a76002dd955838b030b4eff31ae
roman.gorbatenko <roman.gorbatenko@rtlabs.ru>
2020-12-23 23:29:31 +0300

    Initial commit

---
+++ src/main/java/com/example/jenkinsintegratedexample/JenkinsIntegratedExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsintegratedexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsIntegratedExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsIntegratedExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsintegratedexample/JenkinsIntegratedExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsintegratedexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsIntegratedExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
